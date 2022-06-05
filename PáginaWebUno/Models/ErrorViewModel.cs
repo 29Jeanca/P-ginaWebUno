ff6afcfb82a42e9fe0299434ccf40c81fcb7df7a
29Jeanca <105949714+29Jeanca@users.noreply.github.com>
2022-06-04 19:19:11 -0600

    Agregar archivos de proyecto.

---
+++ PáginaWebUno/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PáginaWebUno.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
